724d2d2d274c9cffe85dad3817ebe6183f678179
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-05-23 18:46:50 +0200

    Inicialize, first working project

---
+++ src/main/java/com/WCY20IJ1S1/Casino/CasinoApplication.java
@@ -0,0 +1,13 @@
+package com.WCY20IJ1S1.Casino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoApplication.class, args);
+	}
+
+}
