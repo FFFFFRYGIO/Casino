39a13fab415a7181e56099e545a8d04752972fa6
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-05-23 19:08:48 +0200

    add sample mapping to home page

--- src/main/java/com/WCY20IJ1S1/Casino/CasinoApplication.java
+++ src/main/java/com/WCY20IJ1S1/Casino/CasinoApplication.java
@@ -2,12 +2,19 @@ package com.WCY20IJ1S1.Casino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CasinoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CasinoApplication.class, args);
 	}
 
+	@GetMapping("/home")
+	public String home() {
+		return "Witaj na stronie głównej!";
+	}
 }
