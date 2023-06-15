7c2e94f07547ff35b79b85e5571e74f7876d6814
FFFFFRYGIO <42962305+FFFFFRYGIO@users.noreply.github.com>
2023-06-15 16:20:46 +0200

    fix pull bug, add back button

--- casino-react-app/src/components/Profile/Profile.js
+++ casino-react-app/src/components/Profile/Profile.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './Profile.css';
 import axios from 'axios';
-import { useLocation, useNavigate } from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 
 
-const Profile = () =>{
+const Profile = () => {
 
     const [UserNick, setUserNick] = useState("");
     const [UserBalance, setUserBalance] = useState(0.0);
@@ -21,7 +21,7 @@ const Profile = () =>{
     useEffect(() => {
         axios.get(`/DB/user/get/${ResponseNickName}`)
             .then(response => {
-                const { nickName, balance, history} = response.data;
+                const {nickName, balance, history} = response.data;
                 setUserNick(nickName);
                 setUserBalance(balance);
                 setHistory(history);
@@ -29,15 +29,15 @@ const Profile = () =>{
 
             })
             .catch(error => {
-                 console.error(error);
-                 setLoading(false);
+                console.error(error);
+                setLoading(false);
             });
     }, [ResponseNickName]);
 
-    const deleteUser = () =>{
+    const deleteUser = () => {
         axios.delete(`/DB/user/delete/${ResponseNickName}`)
             .catch(error => {
-                 console.error(error);
+                console.error(error);
             });
         navigate(`/HomePage`);
     }
@@ -46,8 +46,8 @@ const Profile = () =>{
         const divCount = 20;
         const divs = [];
         console.log(history);
-        if(history.length > 0){
-            for (let i = history.length-1; i >= 0; i--) {
+        if (history.length > 0) {
+            for (let i = history.length - 1; i >= 0; i--) {
                 divs.push(<div key={i} class="Payment">
                     <div>Name: {history[i].name}</div>
                     <div>Income: {history[i].income} $</div>
@@ -57,7 +57,7 @@ const Profile = () =>{
         }
         return divs;
     };
-    
+
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         try {
@@ -73,13 +73,12 @@ const Profile = () =>{
         }
     };
 
-    const handleGoBack = () =>{
+    const handleGoBack = () => {
         navigate(`/StartPage?ResponseNickName=${ResponseNickName}`);
     };
 
-    
-    
-    return(
+
+    return (
         <div className="content profile-main">
             <div className='userDataContainer'>
                 <div className="user-data">
@@ -89,40 +88,38 @@ const Profile = () =>{
                     <div className='userName'>User nickName: {UserNick}</div>
                     <div className='Balance'>User balance: {UserBalance}$</div>
                     <form onSubmit={handleFormSubmit}>
-                    <input
-                        type="text"
-                        name="nickName"
-                        placeholder="Input Your New Nick"
-                        required
-                    />
-                    <div className="profile-options">
-                        <button className="chip-button" type="submit">
-                            <img src="img/profile/save-button.png" alt="save" className="option-icon"/>
-                        </button>
-                        <button className="chip-button" onClick={deleteUser}>
-                            <img src="img/profile/delete-button.png" alt="delete" className="option-icon"/>
-                        </button>
-                    </div>
+                        <input
+                            type="text"
+                            name="nickName"
+                            placeholder="Input Your New Nick"
+                            required
+                        />
+                        <div className="profile-options">
+                            <button className="chip-button" onClick={deleteUser}>
+                                <img src="img/profile/go-back-button.png" alt="go_back" className="option-icon"/>
+                            </button>
+                            <button className="chip-button" type="submit">
+                                <img src="img/profile/save-button.png" alt="save" className="option-icon"/>
+                            </button>
+                            <button className="chip-button" onClick={deleteUser}>
+                                <img src="img/profile/delete-button.png" alt="delete" className="option-icon"/>
+                            </button>
+                        </div>
                     </form>
                 </div>
-<<<<<<< HEAD
-                <div style={{ overflow: 'auto', maxHeight: '300px' }} class="history">
+                <div style={{overflow: 'auto', maxHeight: '300px'}} class="history">
                     REGISTRY
                     {generateDivs()}
-=======
-                
-                <div>
-                    <div class="center">
-                        REGISTRY
+                    <div>
+                        <div class="center">
+                            REGISTRY
+                        </div>
+                        <div style={{overflow: 'auto', maxHeight: '300px'}} class="history">
+                            {generateDivs()}
+                        </div>
                     </div>
-                    <div style={{ overflow: 'auto', maxHeight: '300px' }} class="history">
-                        {generateDivs()}
-                    </div>
->>>>>>> e356eb9c2dc5f2fdc16bfc26708c8ee2961007d0
                 </div>
-                
             </div>
-
         </div>
     );
 }
